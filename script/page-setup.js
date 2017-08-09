a14e07c15b28c7737adb088592fddcbe92570710
soccerJoshNumberNine <blue@smittycraft.com>
2017-08-09 12:41:57 -0700

    Update page-setup.js

--- script/page-setup.js
+++ script/page-setup.js
@@ -1,11 +1,17 @@
 document.body.onload = function () {
   createNavigation();
+  fillInSubs();
 };
 
 var links = [
   {
     name: 'Home',
     url: '/'
+  },
+  
+  {
+    name: 'Subscribe',
+    url: 'https://www.youtube.com/channel/UCMm79jobVpVvghnA9voTtag'
   }
 ];
 
@@ -14,5 +20,14 @@ function createNavigation () {
   
   for (i = 0; i < links.length; i++) {
     document.getElementById('nav-bar').innerHTML += '<a href="' + links[i].url + '"><div class="w3-button w3-bar-item">' + links[i].name + '</div></a>';
+  }
+}
+
+function fillInSubs () {
+  var i = 0;
+  var subCount = 63;
+  
+  for (i = 0; i < document.getElementsByClassName('sub-count').length; i++) {
+    document.getElementsByClassName('sub-count')[i].innerHTML = subCount;
   }
 }
