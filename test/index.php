004d8634df99a5cd9c93c9200e3f92f5178ee8d7
soccerJoshNumberNine <blue@smittycraft.com>
2017-08-09 18:10:17 -0700

    Create index.php

---
+++ test/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+  <p>
+    <?php
+      function printName ($name) 
+        echo $name;
+      }
+    ?>
+  </p>
+</body>
+</html>
